0c5a9ae04b01d70edc9a87ef22331c960513693f
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-22 14:07:15 +0300

    gerekli kodlamalar yapıldı

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,43 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // KODLAR BURAYA
-}
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where({ id }).first();
+};
 
-const create = account => {
+const create = (account) => {
   // KODLAR BURAYA
-}
+  return db("accounts")
+    .insert(account)
+    .then((ids) => {
+      return getById(ids[0]);
+    });
+};
 
 const updateById = (id, account) => {
   // KODLAR BURAYA
-}
+  return db("accounts")
+    .where({ id })
+    .update(account)
+    .then(() => {
+      return getById(id);
+    });
+};
 
-const deleteById = id => {
+const deleteById = (id) => {
   // KODLAR BURAYA
-}
+  return getById(id).then((account) => {
+    return db("accounts")
+      .where("id", id)
+      .del()
+      .then(() => account);
+  });
+};
 
 module.exports = {
   getAll,
@@ -24,4 +45,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
